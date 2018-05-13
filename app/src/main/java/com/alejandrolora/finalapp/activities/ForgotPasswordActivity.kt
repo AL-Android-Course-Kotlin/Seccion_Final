2d9d66e74bd7f073705fff14fcbc2c0207bcf880
Alejandro <alejandrofpo@gmail.com>
2018-05-13 16:43:04 +0200

    .

--- app/src/main/java/com/alejandrolora/finalapp/activities/ForgotPasswordActivity.kt
+++ app/src/main/java/com/alejandrolora/finalapp/activities/ForgotPasswordActivity.kt
@@ -1,20 +1,44 @@
 package com.alejandrolora.finalapp.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.alejandrolora.finalapp.R
-import com.alejandrolora.finalapp.goToActivity
+import com.alejandrolora.finalapp.*
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_forgot_password.*
 
 class ForgotPasswordActivity : AppCompatActivity() {
+
+    private val mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
 
+        editTextEmail.validate {
+            editTextEmail.error = if (isValidEmail(it)) null else "Email is not valid"
+        }
+
         buttonGoLogIn.setOnClickListener {
-            goToActivity<LoginActivity>()
+            goToActivity<LoginActivity> {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
+
+        buttonForgot.setOnClickListener {
+            val email = editTextEmail.text.toString()
+            if (isValidEmail(email)) {
+                mAuth.sendPasswordResetEmail(email).addOnCompleteListener(this) {
+                    toast("Email has been sent to reset your password.")
+                    goToActivity<LoginActivity> {
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    }
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                }
+            } else {
+                toast("Please make sure the email address is correct.")
+            }
         }
     }
 }
