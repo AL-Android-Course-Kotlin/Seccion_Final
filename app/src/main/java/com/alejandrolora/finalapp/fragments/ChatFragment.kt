5900ec8a1de6443b5200b626c38ce91f8d81b22f
Alejandro <alejandrofpo@gmail.com>
2018-05-27 18:42:40 +0200

    .

--- app/src/main/java/com/alejandrolora/finalapp/fragments/ChatFragment.kt
+++ app/src/main/java/com/alejandrolora/finalapp/fragments/ChatFragment.kt
@@ -2,17 +2,75 @@ package com.alejandrolora.finalapp.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.alejandrolora.finalapp.R
+import com.alejandrolora.finalapp.adapters.ChatAdapter
+import com.alejandrolora.finalapp.models.Message
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_chat.*
+import kotlinx.android.synthetic.main.fragment_chat.view.*
+import java.util.*
 
 class ChatFragment : Fragment() {
 
+    private lateinit var _view: View
+    private lateinit var adapter: ChatAdapter
+    private val messageList: ArrayList<Message> = ArrayList()
+
+    private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private lateinit var currentUser: FirebaseUser
+
+    private val store: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private lateinit var chatDBRef: CollectionReference
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        _view = inflater.inflate(R.layout.fragment_chat, container, false)
+
+        setUpChatDB()
+        setUpCurrentUser()
+        setUpRecyclerView()
+        setUpChatBtn()
+
+        return _view
+    }
+
+    private fun setUpChatDB() {
+        chatDBRef = store.collection("chat")
+    }
+
+    private fun setUpCurrentUser() {
+        currentUser = mAuth.currentUser!!
+    }
+
+    private fun setUpRecyclerView() {
+        val layoutManager = LinearLayoutManager(context)
+        adapter = ChatAdapter(messageList, currentUser.uid)
+
+        _view.recyclerView.setHasFixedSize(true)
+        _view.recyclerView.layoutManager = layoutManager
+        _view.recyclerView.itemAnimator = DefaultItemAnimator()
+        _view.recyclerView.adapter = adapter
+    }
+
+    private fun setUpChatBtn() {
+        _view.buttonSend.setOnClickListener {
+            val messageText = _view.editTextMessage.text.toString()
+            if(messageText.isNotEmpty()) {
+                val message = Message(currentUser.uid, messageText, currentUser.photoUrl.toString(), Date())
+                // guardaremos el mensaje en firebase
+                _view.editTextMessage.setText("")
+            }
+        }
     }
 
 }
