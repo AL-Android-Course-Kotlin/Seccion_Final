c23bd0740707e1689aaf033dff6b33247ae9c8e0
Alejandro <alejandrofpo@gmail.com>
2018-06-03 13:18:24 +0200

    .

--- app/src/main/java/com/alejandrolora/finalapp/adapters/ChatAdapter.kt
+++ app/src/main/java/com/alejandrolora/finalapp/adapters/ChatAdapter.kt
@@ -42,8 +42,13 @@ class ChatAdapter(val items: List<Message>, val userId: String) : RecyclerView.A
         fun bind(message: Message) = with(itemView) {
             textViewMessageRight.text = message.message
             textViewTimeRight.text = SimpleDateFormat("hh:mm").format(message.sentAt)
-            Picasso.get().load(message.profileImageURL).resize(100, 100)
-                    .centerCrop().transform(CircleTransform()).into(imageViewProfileRight)
+            if(message.profileImageURL.isEmpty()) {
+                Picasso.get().load(R.drawable.ic_person).resize(100, 100)
+                        .centerCrop().transform(CircleTransform()).into(imageViewProfileRight)
+            } else {
+                Picasso.get().load(message.profileImageURL).resize(100, 100)
+                        .centerCrop().transform(CircleTransform()).into(imageViewProfileRight)
+            }
         }
     }
 
@@ -51,8 +56,13 @@ class ChatAdapter(val items: List<Message>, val userId: String) : RecyclerView.A
         fun bind(message: Message) = with(itemView) {
             textViewMessageLeft.text = message.message
             textViewTimeLeft.text = SimpleDateFormat("hh:mm").format(message.sentAt)
-            Picasso.get().load(message.profileImageURL).resize(100, 100)
-                    .centerCrop().transform(CircleTransform()).into(imageViewProfileLeft)
+            if(message.profileImageURL.isEmpty()) {
+                Picasso.get().load(R.drawable.ic_person).resize(100, 100)
+                        .centerCrop().transform(CircleTransform()).into(imageViewProfileLeft)
+            } else {
+                Picasso.get().load(message.profileImageURL).resize(100, 100)
+                        .centerCrop().transform(CircleTransform()).into(imageViewProfileLeft)
+            }
         }
     }
 
--- app/src/main/java/com/alejandrolora/finalapp/fragments/ChatFragment.kt
+++ app/src/main/java/com/alejandrolora/finalapp/fragments/ChatFragment.kt
@@ -70,7 +70,8 @@ class ChatFragment : Fragment() {
         _view.buttonSend.setOnClickListener {
             val messageText = _view.editTextMessage.text.toString()
             if (messageText.isNotEmpty()) {
-                val message = Message(currentUser.uid, messageText, currentUser.photoUrl.toString(), Date())
+                val photo = currentUser.photoUrl?.let { currentUser.photoUrl.toString() } ?: run { "" }
+                val message = Message(currentUser.uid, messageText, photo, Date())
                 saveMessage(message)
                 _view.editTextMessage.setText("")
             }
